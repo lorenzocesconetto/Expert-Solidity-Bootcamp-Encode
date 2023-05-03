990574c5818af118328d7c8b39dc17bd68378174
Lorenzo C <lorenzo@deepcellbio.com>
2023-05-03 17:45:20 -0300

    Fix HW6

--- Homework6.sol
+++ Homework6.sol
@@ -5,7 +5,8 @@ pragma solidity 0.8.17;
 contract Homework6 {
     function returnEth() public payable returns (uint) {
         assembly {
-            return(0x00, 32)
+            mstore(0x00, callvalue())
+            return(0x0, 0x20)
         }
     }
 }
